c1dfa93ef0f76bbd793155a7cd33ec1baa41d810
kji0205 <kji0205@gmail.com>
2016-06-07 22:34:03 +0900

    Tests

---
+++ ASPNET/Controllers/AdminController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ASPNET.TestingDemo.Models;
+
+namespace ASPNET.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IUserRepository repository;
+        
+        public AdminController(IUserRepository repo)
+        {
+            repository = repo;
+        }    
+
+        public ActionResult ChangeLoginName(String oldName, string newName)
+        {
+            User user = repository.FetchByLoginName(oldName);
+            user.LoginName = newName;
+            repository.SubmitChanges();
+            return View();
+        }
+    }
+}
---
+++ ASPNET/Models/TestingDemo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASPNET.TestingDemo.Models
+{
+    public class TestingDemo
+    {
+    }
+
+    public class User
+    {
+        public string LoginName { get; set; }
+    }
+
+    public interface IUserRepository
+    {
+        void Add(User newUser);
+        User FetchByLoginName(string loginName);
+        void SubmitChanges();
+    }
+
+    public class DefaultUserRepository : IUserRepository
+    {
+        public void Add(User newUser)
+        {
+            // To do
+        }
+        public User FetchByLoginName(string loginName)
+        {
+            // To do
+            return new User() { LoginName = loginName };
+        }
+        public void SubmitChanges()
+        {
+            // To do
+        }
+    }
+}
