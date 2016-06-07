4dd0e4d89d68d2835684bd1e995cf2b7a1398bfb
kji0205 <kji0205@gmail.com>
2016-06-07 21:46:11 +0900

    RSVP

--- ASPNET/Controllers/HomeController.cs
+++ ASPNET/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ASPNET.Models;
 
 namespace ASPNET.Controllers
 {
@@ -13,6 +14,32 @@ namespace ASPNET.Controllers
             int hour = DateTime.Now.Hour;
             ViewData["greeting"] = (hour < 12 ? "Good morning" : "Good afternoon");
             return View();
+        }
+
+        public ViewResult Index2()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
         }
 
         public ActionResult About()
