84f508e701acaac1ad3370889b9f75bdfb278402
kji0205 <kji0205@gmail.com>
2016-06-09 00:27:50 +0900

    EssentialTools

---
+++ ASPNET/Controllers/EssentialToolsController.cs
@@ -0,0 +1,41 @@
+﻿using ASPNET.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Ninject;
+
+namespace ASPNET.Controllers
+{
+    public class EssentialToolsController : Controller
+    {
+        private IValueCalculator calc;
+        private Product[] products =
+        {
+            new Product {Name="Kayak", Category="Watersports", Price=275M },
+            new Product {Name="Lifejacket", Category="Watersports", Price=48.95M },
+            new Product {Name="Soccer ball", Category="Soccer", Price=19.50M },
+            new Product {Name="Corner flag", Category="Soccer", Price=34.95M }
+        };
+
+        public EssentialToolsController(IValueCalculator calcParam)
+        {
+            calc = calcParam;
+        }
+
+        // GET: EssentialTools
+        public ActionResult Index()
+        {
+            IKernel ninjectKernel = new StandardKernel();
+            ninjectKernel.Bind<IValueCalculator>().To<LinqValueCalculator>();
+            IValueCalculator calc = ninjectKernel.Get<IValueCalculator>();
+
+            //LinqValueCalculator calc = new LinqValueCalculator();
+
+            ShoppingCart2 cart = new ShoppingCart2(calc) { Products = products };
+            decimal totalValue = cart.CalculateProductTotal();
+            return View(totalValue);
+        }
+    }
+}
---
+++ ASPNET/Models/EssentialTools.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ASPNET.Models
+{
+    public class LinqValueCalculator : IValueCalculator
+    {
+        private IDiscountHelper discounter;
+        public LinqValueCalculator(IDiscountHelper discountParam)
+        {
+            discounter = discountParam;
+        }
+        public decimal ValueProducts(IEnumerable<Product> products)
+        {
+            //return products.Sum(p => p.Price);
+            return discounter.ApplyDiscount(products.Sum(p => p.Price));
+        }
+    }
+
+    public class ShoppingCart2
+    {
+        private IValueCalculator calc;
+        public ShoppingCart2(IValueCalculator calcParam)
+        {
+            calc = calcParam;
+        }
+        public IEnumerable<Product> Products { get; set; }
+        public decimal CalculateProductTotal()
+        {
+            return calc.ValueProducts(Products);
+        }
+    }
+
+    public interface IValueCalculator
+    {
+        decimal ValueProducts(IEnumerable<Product> products);
+    }
+
+    public interface IDiscountHelper
+    {
+        decimal ApplyDiscount(decimal totalParam);
+    }
+
+    public class DefaultDiscountHelper : IDiscountHelper
+    {
+        public decimal discountSize;
+        public DefaultDiscountHelper(decimal discountParam)
+        {
+            discountSize = discountParam;
+        }
+        public decimal ApplyDiscount(decimal totalParam)
+        {
+            return (totalParam - (discountSize / 100m * totalParam));
+        }
+    }
+
+    public class FlexibleDiscountHelper : IDiscountHelper
+    {
+        public decimal ApplyDiscount(decimal totalParam)
+        {
+            decimal discount = totalParam > 100 ? 70 : 25;
+            return (totalParam - (discount / 100m * totalParam));
+        }
+    }
+
+}
