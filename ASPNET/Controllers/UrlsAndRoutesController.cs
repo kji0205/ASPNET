8c4b55adcbd609f8e3b5e7c2addd295d538b796d
kji0205 <kji0205@gmail.com>
2016-06-16 00:18:33 +0900

    Make Controller Project

---
+++ ASPNET/Controllers/UrlsAndRoutesController.cs
@@ -0,0 +1,37 @@
+﻿using System.Web.Mvc;
+
+namespace ASPNET.Controllers
+{
+    public class UrlsAndRoutesController : Controller
+    {
+        // GET: UrlsAndRoutes
+        public ActionResult Index()
+        {
+            ViewBag.Controller = "Home";
+            ViewBag.Action = "Index";
+            return View("ActionName");
+        }
+
+        public ActionResult Customer_Index()
+        {
+            ViewBag.Controller = "Customer";
+            ViewBag.Action = "Index";
+            return View("ActionName");
+        }
+
+        public ActionResult Customer_List()
+        {
+            ViewBag.Controller = "Customer";
+            ViewBag.Action = "List";
+            return View("ActionName");
+        }
+
+        public ActionResult Admin_Index()
+        {
+            ViewBag.Controller = "Admin";
+            ViewBag.Action = "Index";
+            return View("ActionName");
+        }
+    }
+    
+}
--- ControllersAndActions/App_Start/RouteConfig.cs
+++ ControllersAndActions/App_Start/RouteConfig.cs
@@ -5,20 +5,13 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 
-namespace ASPNET
+namespace ControllersAndActions
 {
     public class RouteConfig
     {
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
-            //Route myRoute = new Route("{controller}/{action}", new MvcRouteHandler());
-            //routes.Add("MyRoute", myRoute);
-
-            //routes.MapRoute("MyRoute", "{controller}/{action}/{id}/{*catchall}",
-            //    new { controller="Home", action = "Index", id=UrlParameter.Optional },
-            //     new[] { "URLsAndRoutes.AdditionalControllers" });
 
             routes.MapRoute(
                 name: "Default",
