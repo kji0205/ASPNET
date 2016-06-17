0eeae86934f22b280025cdff6931eade85d1f12e
kji0205 <kji0205@gmail.com>
2016-06-18 01:49:44 +0900

    C17 F

--- ControllersAndActions.Tests/UnitTest1.cs
+++ ControllersAndActions.Tests/UnitTest1.cs
@@ -11,8 +11,21 @@ namespace ControllersAndActions.Tests
         public void ControllerTest()
         {
             ExampleController target = new ExampleController();
-            ViewResult result = target.Index();
-            Assert.AreEqual("Homepage", result.ViewName);
+            //ViewResult result = target.Index();
+            //RedirectResult result = target.Redirect();
+            RedirectToRouteResult result = target.Redirect();
+            //Assert.AreEqual("Homepage", result.ViewName);
+            //Assert.IsTrue(result.Permanent);
+            //Assert.AreEqual("/Example/Index", result.Url);
+
+            Assert.IsFalse(result.Permanent);
+            //Assert.AreEqual("Example", result.RouteValues["controller"]);
+            Assert.AreEqual("Index", result.RouteValues["action"]);
+            //Assert.AreEqual("MyID", result.RouteValues["ID"]);
+
+            HttpStatusCodeResult result2 = target.StatusCode401();
+            Assert.AreEqual(401, result2.StatusCode);
+
 
         }
 
@@ -24,5 +37,6 @@ namespace ControllersAndActions.Tests
             Assert.AreEqual("", result.ViewName);
             Assert.IsInstanceOfType(result.ViewData.Model, typeof(System.DateTime));
         }
+        
     }
 }
--- ControllersAndActions/Controllers/ExampleController.cs
+++ ControllersAndActions/Controllers/ExampleController.cs
@@ -8,8 +8,43 @@ namespace ControllersAndActions.Controllers
         // GET: Example
         public ViewResult Index()
         {
-            DateTime date = DateTime.Now;
-            return View(date);
+            //DateTime date = DateTime.Now;
+            ViewBag.Message = TempData["Message"];
+            ViewBag.Date = TempData["Date"];
+            return View();
+        }
+
+        //public RedirectResult Redirect()
+        //{
+        //    //return Redirect("/Example/Index");
+        //    return RedirectPermanent("/Example/Index");
+        //}
+
+        //public RedirectToRouteResult Redirect()
+        //{
+        //    return RedirectToRoute(new
+        //    {
+        //        controller = "Example",
+        //        action = "Index",
+        //        ID = "MyID"
+        //    });
+        //}
+
+        public RedirectToRouteResult Redirect()
+        {
+            TempData["Message"] = "Hello";
+            TempData["Date"] = DateTime.Now;
+            return RedirectToAction("Index");
+        }
+
+        public HttpStatusCodeResult StatusCode()
+        {
+            return new HttpStatusCodeResult(404, "URL cannot be serviced");
+        }
+
+        public HttpStatusCodeResult StatusCode401()
+        {
+            return new HttpUnauthorizedResult();
         }
     }
 }
